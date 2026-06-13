3901f6a9ac9b0f4f087fb8d6d5f1daafe9d621a5
NEGO2522 <borbreak@gmail.com>
2026-06-14 01:00:33 +0530

    Update the match

--- src/components/SplitText.jsx
+++ src/components/SplitText.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-export const SplitText = ({ text }) => (
-  <>
-    {text.split('').map((char, i) => (
-      char === ' ' ? <span key={i}>&nbsp;</span> : (
-        <span key={i} className="hl" data-char={char}>
-          <span className="hl-inner">{char}</span>
-        </span>
-      )
-    ))}
-  </>
-);
+export const SplitText = ({ text }) => {
+  return (
+    <>
+      {text.split(' ').map((word, wordIndex, wordsArray) => (
+        <React.Fragment key={wordIndex}>
+          <span style={{ whiteSpace: 'nowrap' }}>
+            {word.split('').map((char, charIndex) => (
+              <span key={charIndex} className="hl" data-char={char}>
+                <span className="hl-inner">{char}</span>
+              </span>
+            ))}
+          </span>
+          {wordIndex < wordsArray.length - 1 && ' '}
+        </React.Fragment>
+      ))}
+    </>
+  );
+};
